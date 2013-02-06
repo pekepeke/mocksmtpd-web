efe6628c9854b5aba28214c45440daa5af6083ca
pekepeke <pekepekesamurai@gmail.com>
2013-02-06 17:53:28 +0900

    fix navbar

--- web/app.rb
+++ web/app.rb
@@ -132,13 +132,13 @@ __END__
     <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <a class="btn btn-navbar" data-toggle="collapse" data-bypass="" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </a>
-          <a class="brand" href="/">MockSMTPD</a>
-          <div class="nav-collapse">
+          <a class="brand hidden-desktop" href="/">MockSMTPD</a>
+          <div class="nav-collapse collapse">
             <ul class="nav">
               <li <%= @nav.nil? ? 'class="active"' : "" %>><a href="/"><i class="icon-envelope icon-white"></i> Inbox</a></li>
               <li class="dropdown">
