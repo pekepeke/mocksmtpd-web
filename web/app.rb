7233526454ef9ba69c55f3789766628ec5c18f80
pekepeke <pekepekesamurai@gmail.com>
2013-02-21 14:34:23 +0900

    impl autoreload

--- web/app.rb
+++ web/app.rb
@@ -133,6 +133,20 @@ __END__
               }
             }
           });
+          (function (w, r, c) {
+            w['r'+r] = w['r'+r] || w['webkitR'+r] || w['mozR'+r] || w['msR'+r] || w['oR'+r] || function(f){ w.setTimeout(f, 1000 / 60); };
+            w['c'+c] = w['c'+c] || w['webkitC'+c] || w['mozC'+c] || w['msC'+c] || w['oC'+c] || function(t){ w.clearTimeout(t); };
+          })(window, 'equestAnimationFrame', 'ancelRequestAnimationFrame');
+          var timer_id = null;
+          setInterval(function() {
+            if (location.pathname == "/") {
+              cancelRequestAnimationFrame(timer_id);
+              timer_id = requestAnimationFrame(function() {
+                $('.nav li:first a').trigger('click');
+                // console.log("reload" + new Date().toString())
+              });
+            }
+          }, 5000);
         })
       }(jQuery, this);
 //]]></script>
