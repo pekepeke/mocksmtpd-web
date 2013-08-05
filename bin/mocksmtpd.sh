5bc78c2371722ece9e1cd791e4f8f2a6fa444003
pekepeke <pekepekesamurai@gmail.com>
2013-08-05 14:12:39 +0900

    fix bin script

--- bin/mocksmtpd.sh
+++ bin/mocksmtpd.sh
@@ -4,5 +4,7 @@ cd $(dirname $0)/..
 
 MOCKSMTPD_BIN="/usr/bin/ruby -Imocksmtpd/lib mocksmtpd/bin/mocksmtpd"
 
+[ -e log/mocksmtpd.pid ] && kill -0 $(cat log/mocksmtpd.pid) || rm log/mocksmtpd.pid
+
 exec $MOCKSMTPD_BIN -f conf/mocksmtpd.conf
 
